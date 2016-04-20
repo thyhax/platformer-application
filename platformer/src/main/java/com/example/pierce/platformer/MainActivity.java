11ec5fb35aab5694f5d3d3e9c25ad8cdfb8d7762
Pierce <adajarp@iastate.edu>
2016-04-20 13:46:47 -0500

    Added template Activity to start AndroidGame

--- platformer/src/main/java/com/example/pierce/platformer/MainActivity.java
+++ platformer/src/main/java/com/example/pierce/platformer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pierce.platformer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,24 +9,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.platformer.framework.implementation.AndroidGame;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    }
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void newGame(View view) {
+        Intent intent = new Intent(this, AndroidGame.class);
+        startActivity(intent);
     }
 
     @Override
